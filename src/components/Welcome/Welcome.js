1afc59cd7c1ccae99674f81f347059427499a923
Dwayne <flipguy19@gmail.com>
2019-04-27 13:23:58 -0400

    Added some subtext as well as icons for skills section

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,14 +6,15 @@ class Welcome extends React.Component {
   render() {
     return(
       <div className="welcome">
-          <Container>
-            <Row className="justify-content-center">
-              <Col xs="auto">
-                <Button variant="primary" size="lg" className="button" href="#">Check me out</Button>
-                <h1 className="hello"> Hello My name is Harrison Jones, <br></br>I am a Junior Front end Devloper</h1>
-              </Col>
-            </Row>
-          </Container>
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs="auto">
+              <Button variant="primary" size="lg" className="button" href="#">Check me out</Button>
+              <h1 className="hello"> Hello My name is Harrison Jones, <br></br>I am a Junior Front end Devloper</h1>
+            </Col>
+          </Row>
+          
+        </Container>
       </div>
     );
   }
