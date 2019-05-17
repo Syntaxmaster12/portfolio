a3a4f430c1af102a6823998de398205d0f18d5a5
Dwayne <flipguy19@gmail.com>
2019-05-17 10:27:16 -0400

    changed minor things

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ export default class About extends React.Component {
           <Row className="justify-content-center">
             <Col xs="auto">
               <div className="profile-pic mr-auto ml-auto"></div>
-              <p className="about-me mt-4">My name is Harrison Jones, I am from Philadelphia born and raised here. I am very passionate about code and anything related to it. I’ve always been the type of guy that wants to know how things work and why they work the way do, from cars to software I am interested at the deepest level on how things tick and thats probably why I am so interested in web development. Some things I love are tea, code ( of course ), cars or really anything with an engine that revs high and goes fast.</p>
+              <p className="about-me mt-4">My name is Harrison Jones, I am from Philadelphia born and raised here. I am very passionate about code and anything related to it. I’ve always been the type of guy that wants to know how things work and why they work the way do, from cars to software I am interested at the deepest level on how things tick and that's probably why I am so interested in web development. Some things I love are tea, code ( of course ), cars or really anything with an engine that revs high and goes fast.</p>
             </Col>
           </Row>
           </div>
