3fe3228ea0130355ce13ff22f1566afcf8bf84fc
Harrison Jones <harrisonjones@Harrisons-iMac.local>
2019-05-29 12:57:14 -0400

    fixed grammer

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -10,7 +10,7 @@ class Welcome extends React.Component {
           <Row className="justify-content-center welcome-row">
             <Col xs="auto">
               <h1 className="hello"> My name is Harrison Jones,</h1>
-              <h3 className="hello-2">I am a Front end Devloper</h3>
+              <h3 className="hello-2">I am a Front End Devloper</h3>
             </Col>
           </Row>
         </Container>
